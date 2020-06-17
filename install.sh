1ac8a8390562a81da898a6a2eab350e867b17124
Christian Duerr <contact@christianduerr.com>
2020-06-17 04:36:53 +0200

    Refactor color variables

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-cat userContent/* > ~/.mozilla/firefox/*/chrome/userContent.css
-cat userChrome/* > ~/.mozilla/firefox/*/chrome/userChrome.css
+cat variables.css > ~/.mozilla/firefox/*/chrome/userContent.css
+cat variables.css > ~/.mozilla/firefox/*/chrome/userChrome.css
+
+cat userContent/* >> ~/.mozilla/firefox/*/chrome/userContent.css
+cat userChrome/* >> ~/.mozilla/firefox/*/chrome/userChrome.css
