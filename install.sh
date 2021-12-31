fe563eb9125973558e14bba8cc271137ced262d0
Christian Duerr <contact@christianduerr.com>
2022-01-01 00:57:26 +0100

    Write styles to all firefox profiles

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cat variables.css > ~/.mozilla/firefox/*/chrome/userContent.css
-cat variables.css > ~/.mozilla/firefox/*/chrome/userChrome.css
+cat variables.css | tee ~/.mozilla/firefox/*/chrome/userContent.css > /dev/null
+cat variables.css | tee ~/.mozilla/firefox/*/chrome/userChrome.css > /dev/null
 
-cat userContent/* >> ~/.mozilla/firefox/*/chrome/userContent.css
-cat userChrome/* >> ~/.mozilla/firefox/*/chrome/userChrome.css
+cat userContent/* | tee -a ~/.mozilla/firefox/*/chrome/userContent.css > /dev/null
+cat userChrome/* | tee -a ~/.mozilla/firefox/*/chrome/userChrome.css > /dev/null
